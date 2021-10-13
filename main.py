689377a09b25d73a64a5e9f724c2ad2197aa1428
Semyon Novikov <semyon-dev@protonmail.com>
2021-10-13 21:20:03 +0300

    add parsing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import datetime
-
+import sys
 import pymongo
 import certifi
 import os
@@ -17,14 +17,6 @@ client = pymongo.MongoClient(CONNECTION_STRING, tlsCAFile=certifi.where())
 db = client.main
 data_collection = db.data
 
-
-def insert_doc(doc):
-    try:
-        data_collection.insert_one(doc)
-    except Exception as e:
-        print(e)
-
-
 month_to_number = {
     'Январь': 1,
     'Февраль': 2,
@@ -40,6 +32,40 @@ month_to_number = {
     'Декабрь': 12
 }
 
+
+def insert_doc(doc):
+    try:
+        data_collection.insert_one(doc)
+    except Exception as e:
+        print(e)
+        sys.exit()
+
+
+def parse_format(items: list):
+    arr: list = []
+    for elem in items:
+        # пропускаем плохие данные
+        if ".." in elem:
+            continue
+        parsed_item: dict = {
+            "n": '',
+        }
+        for e in range(len(elem)):
+            if elem[e].isdigit():
+                parsed_item["n"] += elem[e]
+            else:
+                parsed_item["X"] = elem[e]
+                parsed_item["Y"] = elem[e + 1]
+                if elem[e + 2:] != "":
+                    parsed_item["m"] = elem[e + 2:]
+                    parsed_item["m"] = str.removesuffix(parsed_item["m"], "д")
+                break
+        if parsed_item["n"] != '':
+            parsed_item["n"] = int(parsed_item["n"])
+        arr.append(parsed_item)
+    return arr
+
+
 if __name__ == '__main__':
 
     with open('data.csv', encoding='utf-8') as csv_file:
@@ -50,48 +76,38 @@ if __name__ == '__main__':
                 line_count += 1
                 continue
             # id, Начало, Длительность, Окончание, Последователи, Предшественники
-            date_start = str.split(row[1], " ")
-            date_end = str.split(row[3], " ")
-            hour_minute_start = str.split(date_start[3], ":")
-            hour_minute_end = str.split(date_end[3], ":")
-            if "," in row[2]:
-                duration_array = str.split(row[2], ",")
-                duration = int(duration_array[0])
-            else:
-                duration = int((row[2])[0:-1])
-
-            followers = str.split(row[4], ";"),
-            predecessors = str.split(row[5], ";"),
-            for elem in followers:
-                for i in range(len(elem)):
-                    if elem[i].isdigit():
-                        continue
-                    else:
-                        print(elem[i])
-
-            for elem in predecessors:
-                for i in range(len(elem)):
-                    if elem[i].isdigit():
-                        continue
-                    else:
-                        print(elem[i])
-
             item = {
                 "_id": int(row[0]),
-                "start": datetime.datetime(int(date_start[2]), month_to_number[date_start[1]], int(date_start[0]),
-                                           int(hour_minute_start[0]),
-                                           int(hour_minute_start[1])),
-                "duration": duration,
-                "end": datetime.datetime(int(date_end[2]), month_to_number[date_end[1]], int(date_end[0]),
-                                         int(hour_minute_end[0]),
-                                         int(hour_minute_end[1])),
-                "followers": str.split(row[4], ";"),
-                "predecessors": str.split(row[5], ";")
             }
+            if row[1] != "":
+                date_start = str.split(row[1], " ")
+                hour_minute_start = str.split(date_start[3], ":")
+                item["start"] = datetime.datetime(int(date_start[2]), month_to_number[date_start[1]],
+                                                  int(date_start[0]),
+                                                  int(hour_minute_start[0]),
+                                                  int(hour_minute_start[1]))
+            if row[2] != "":
+                if "," in row[2]:
+                    duration_array = str.split(row[2], ",")
+                    duration = int(duration_array[0])
+                else:
+                    duration = int((row[2])[0:-1])
+                item["duration"] = duration
+            if row[3] != "":
+                date_end = str.split(row[3], " ")
+                hour_minute_end = str.split(date_end[3], ":")
+                item["end"] = datetime.datetime(int(date_end[2]), month_to_number[date_end[1]], int(date_end[0]),
+                                                int(hour_minute_end[0]),
+                                                int(hour_minute_end[1]))
+            if row[4] != "":
+                followers_mongo = parse_format(str.split(row[4], ";"))
+                item["followers"] = followers_mongo
+            if row[5] != "":
+                predecessors_mongo = parse_format(str.split(row[5], ";"))
+                item["predecessors"] = predecessors_mongo
             # insert_doc(item)
-            if line_count % 50 == 0:
+            if line_count % 100 == 0:
                 print("line: ", line_count)
-                break
             line_count += 1
 
-    print(f'Processed {line_count} lines.')
+    print(f'\nProcessed {line_count} lines.')
